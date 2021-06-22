226d57e7f9a5a0ef5864877754bf69d0bbafc3bc
Iain Duff <iainrobertduff@gmail.com>
2021-06-22 16:00:09 +0400

    Changes...

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, createRef } from "react";
 import gsap from 'gsap';
 import Cursor from '../components/Cursor';
 import ModalContent from "./ModalContent";
+import {mItems} from "./ModalContent";
 
 gsap.registerPlugin()
 
@@ -9,6 +10,7 @@ function Modal(props) {
   const modalRef = createRef();
   const modalScrimRef = createRef();
   const modalCardRef = createRef();
+  const menuItems = createRef();
   const tl = useRef();
 
   useEffect(() => {
@@ -32,16 +34,24 @@ function Modal(props) {
 
   const close = e => {
     e.preventDefault();
+    tl.current = gsap.to('.menu-content-items', { delay: 0, opacity: 0, duration: 0.8, ease: 'power2.inOut', onComplete: changeProps})
+    tl.current = gsap.to(modalRef.current, {delay:0.2, duration: 0.8, opacity: 1, width: '0%', ease: 'power2.inOut'})
+    tl.current = gsap.set(modalScrimRef.current, { delay: 2, opacity: 0, visibility: 'hidden'})
+    tl.current = gsap.set(modalRef.current, { delay:2, opacity: 0, visibility: 'hidden'})
     
-    tl.current.timeScale(2).reversed(props.modal);
   };
+
+  function changeProps() {
+    props.setModal(false);
+  }
+
 
   return (
     <div ref={modalRef} onClick={close} className="modal">
       <Cursor />
       <div ref={modalScrimRef} className="modal__scrim" />
       <div ref={modalCardRef} className="modal__card h-screen flex">
-        {props.modal ? <ModalContent modal={props.modal} /> : null}
+        {props.modal ? <ModalContent ref={menuItems} modal={props.modal} /> : null}
       </div>
     </div>
   );
--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -4,18 +4,14 @@ import { SplitText } from "../components/SplitText";
 
 function ModalContent(props) {
   const contentRef = createRef();
+  const mItems = createRef();
   const tl = useRef();
-
-  
-
-
-  
 
   useEffect(() => {
 
     if (typeof document !== 'undefined') {
 
-    var tlx = gsap.timeline({delay: 0}), 
+    var tlx = gsap.timeline({delay: 0.2}), 
     mySplitText = new SplitText("#menu-items", {type:"words,chars"}), 
     chars = mySplitText.chars; //an array of all the divs that wrap each character
 
@@ -26,8 +22,8 @@ function ModalContent(props) {
   }, []);
 
   return (
-    <div id="menu-items" className="content m-auto text-left ml-40">
-        <p className="text-white list-none text-6xl md:text-6xl lg:text-8xl font-thin relative overflow-hidden uppercase transform phrase hover:text-yellow-700">Biography</p>
+    <div ref={mItems} id="menu-items" className="content menu-content-items m-auto text-left ml-40">
+        <p className="text-white list-none text-6xl md:text-6xl lg:text-8xl font-thin relative overflow-hidden uppercase transform phrase hover:text-yellow-700">Biographie</p>
         <p className="text-white list-none text-6xl md:text-6xl lg:text-8xl font-thin relative overflow-hidden uppercase transform phrase hover:text-red-700">Case Studies</p>
         <p className="text-white list-none text-6xl md:text-6xl lg:text-8xl font-thin relative overflow-hidden uppercase transform phrase hover:text-blue-700">Resume</p>
         <p className="text-white list-none text-6xl md:text-6xl lg:text-8xl font-thin relative overflow-hidden uppercase transform phrase hover:text-green-700">Contact</p>
