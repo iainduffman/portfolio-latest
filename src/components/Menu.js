8e38e5475db58c55a872a8d6df3a61ff1a1b625e
Iain Duff <iainrobertduff@gmail.com>
2021-06-23 17:44:47 +0400

    Updating nav

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -20,8 +20,8 @@ export const Menu = () => {
 
       <div className="flex mb-20 justify-between menu">
       <div className="mr-8 text-md uppercase">
-            <a href="#" onClick={btnClick}>
-              Menu Toggle Placehold
+            <a href="#">
+              Logo
             </a>
             <Drawer modal={modal} setModal={setModal} />
           </div>
@@ -29,72 +29,16 @@ export const Menu = () => {
           
 
 
-        <div className="mr-8 text-xl">
-          <TransitionLink
-            to="/"
-            exit={{
-              length: 2,
-            }}
-            entry={{ length: 1 }}
-            className="hover:text-gray-600"
-          >
-            Logo Placehold
-          </TransitionLink>
-        </div>
+      
         <div className="flex">
           <div className="mr-8 text-md uppercase">
-            <TransitionLink
-              to="/about"
-              exit={{
-                length: 2,
-              }}
-              entry={{ length: 1 }}
-              className="hover:text-gray-600"
-              activeClassName="border-b-2 border-gray-600"
-            >
-              Onee
-            </TransitionLink>
+          <a href="#" onClick={btnClick}>
+              Open Menu
+            </a>
           </div>
-          <div className="mr-8 text-md uppercase">
-            <TransitionLink
-              to="/contact"
-              exit={{
-                length: 2,
-              }}
-              entry={{}}
-              className="hover:text-gray-600"
-              activeClassName="border-b-2 border-gray-600"
-            >
-              Two
-            </TransitionLink>
-          </div>
-          <div className="mr-8 text-md uppercase">
-            <TransitionLink
-              to="/next"
-              exit={{
-                length: 2,
-              }}
-              entry={{}}
-              className="hover:text-gray-600"
-              activeClassName="border-b-2 border-gray-600"
-            >
-              Three
-            </TransitionLink>
-          </div>
-          <div className="mr-8 text-md uppercase">
-            <TransitionLink
-              to="/last"
-              exit={{
-                length: 2,
-              }}
-              entry={{}}
-              className="hover:text-gray-600"
-              activeClassName="border-b-2 border-gray-600"
-            >
-              Five
-            </TransitionLink>
-          </div>
-          <div className="mr-8 text-md uppercase">
+
+
+          {/* <div className="mr-8 text-md uppercase">
             <TransitionLink
             rel="prefetch"
               to="/lastone"
@@ -107,7 +51,9 @@ export const Menu = () => {
             >
               Six
             </TransitionLink>
-          </div>
+          </div> */}
+
+
         </div>
       </div>
     </>
